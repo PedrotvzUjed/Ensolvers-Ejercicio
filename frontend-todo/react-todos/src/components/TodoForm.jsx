380ad2e38fe09b081f6e90a333cc119da0f77efc
pedrotvzujed <pedro.tavarez@ujed.mx>
2022-03-28 21:07:19 -0600

    Todolist

---
+++ frontend-todo/react-todos/src/components/TodoForm.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Form, Row, Col, Button, Input } from 'antd';
+import { PlusCircleFilled } from '@ant-design/icons';
+import renderEmpty from 'antd/lib/config-provider/renderEmpty';
+import FormItem from 'antd/lib/form/FormItem';
+
+const TodoForm = ({ onFormSubmit }) => {
+    const [form] = Form.useForm();
+
+    const onFinish = () => {
+        onFormSubmit({
+            title: form.getFieldValue('title'),
+            completed: false
+        });
+        console.log(form.getFieldValue('title'));
+        form.resetFields();
+    }
+    return (
+        <Form
+            form={form}
+            onFinish={onFinish}
+            layout="horizontal"
+            className='todo-form'>
+            <Row gutter={20}>
+                <Col xs={24} sm={24} md={17} lg={19} xl={20}>
+                    <Form.Item
+                        name={'title'}
+                        rules={[{ required: true, message: 'This fiel is required' }]}>
+                        <Input placeholder='What needs to be done?' />
+                    </Form.Item>
+                </Col>
+                <Col xs={24} sm={24} md={7} lg={5} xl={4}>
+                    <Button type='primary' htmlType='submit' block>
+                        Add Todo
+                    </Button>
+                </Col>
+
+            </Row>
+
+
+
+
+        </Form>
+    );
+}
+export default TodoForm;
